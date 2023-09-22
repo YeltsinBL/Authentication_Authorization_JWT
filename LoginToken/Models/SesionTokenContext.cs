3bce3e624de408041d74535b61633527ffa32e35
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2023-09-21 23:28:25 -0500

    Configuración para la conexión a la Base de datos
    Se utilizó la consola de paquetes de nuget para importar los modelos de las tablas

---
+++ LoginToken/Models/SesionTokenContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginToken.Models;
+
+public partial class SesionTokenContext : DbContext
+{
+    public SesionTokenContext()
+    {
+    }
+
+    public SesionTokenContext(DbContextOptions<SesionTokenContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<HistorialRefreshToken> HistorialRefreshTokens { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<HistorialRefreshToken>(entity =>
+        {
+            entity.HasKey(e => e.IdHistorialToken).HasName("PK__Historia__03DC48A58F302C9A");
+
+            entity.ToTable("HistorialRefreshToken");
+
+            entity.Property(e => e.EsActivo).HasComputedColumnSql("(case when [FechaExpiracion]<getdate() then CONVERT([bit],(0)) else CONVERT([bit],(1)) end)", false);
+            entity.Property(e => e.FechaCreacion).HasColumnType("datetime");
+            entity.Property(e => e.FechaExpiracion).HasColumnType("datetime");
+            entity.Property(e => e.RefreshToken)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.Token)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.HistorialRefreshTokens)
+                .HasForeignKey(d => d.IdUsuario)
+                .HasConstraintName("FK__Historial__IdUsu__267ABA7A");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PK__Usuario__5B65BF975C9660CC");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.NombreUsuario)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- LoginToken/Program.cs
+++ LoginToken/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using LoginToken.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Referencia a la conexión de la base de datos
+builder.Services.AddDbContext<SesionTokenContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("connectionSQL"));
+});
 
 var app = builder.Build();
 
